dd03de1144b08ee1edc371be1cfeb5b79587e21d
crystal <969274162@qq.com>
2020-10-27 21:34:01 +0800

    [fangni] fix: fix cross-domain problem

--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/api/StudentController.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/api/StudentController.java
@@ -3,11 +3,13 @@ package com.thoughtworks.capability.gtb.entrancequiz.api;
 import com.thoughtworks.capability.gtb.entrancequiz.dto.StudentDto;
 import com.thoughtworks.capability.gtb.entrancequiz.service.StudentService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 public class StudentController {
     final
